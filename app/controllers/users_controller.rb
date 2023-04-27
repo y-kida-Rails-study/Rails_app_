8b37eb8f78ae02d9f4236ce94b3c2645a1c53305
EC2 Default User <ec2-user@ip-172-31-7-17.ap-northeast-1.compute.internal>
2023-04-27 12:19:46 +0000

    10-5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   include SessionsHelper
   
@@ -38,6 +38,11 @@ class UsersController < ApplicationController
       flash.now[:danger] = '更新に失敗しました'
       render "edit"
     end
+  end
+  
+  def destroy
+    User.find_by(id: params[:id]).destroy!
+    redirect_to users_url
   end
   
   private
